a4a969026bd2fb74646e41ac5216bc79484a80e3
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2023-08-03 19:38:59 +0530

    Create 25ShallowCopy.cpp

---
+++ 25ShallowCopy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class A
+{
+    int *p;
+    public:A(int x)
+           {
+               p=new int(x);
+           }
+           void set(int x)
+           {
+               *p=x;
+           }
+           void get()
+           {
+               cout<<*p;
+           }
+};
+int main()
+{
+    A a(10);
+    A b(a);
+    b.set(20);
+    a.get();
+    b.get();
+}
